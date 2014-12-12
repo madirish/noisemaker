a80dd9ef406ce21d41f845632a62a16ed99f58ff
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2014-12-12 12:46:06 -0500

    Got ruby version working

--- noisemaker.rb
+++ noisemaker.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/ruby
 
+require 'rubygems'
+require 'nokogiri'
 require 'open-uri'
 
-def initialize_database(location='/usr/share/dict/linux.words')
+def initialize_database(location)
   words = Array.new
   if not File.exist?(location)
     return false
@@ -14,11 +16,87 @@ def initialize_database(location='/usr/share/dict/linux.words')
   return words
 end
 
-words = initialize_database()
-print "Words has #{words.count}\n"
+def randomagentstring() 
+  agents = ["FooBrowser 2.3", 
+        "<script>alert('xss browser');</script>", 
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322; FDM)",
+        "Mozilla/6.0; (Spoofed by Amiga-AWeb/3.5.07 beta)",
+        "Mozilla/3.0 (compatible; MSIE 5.0; Windows NT 4.1; Trident/2.0)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; Trident/3.0)",
+        "Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/3.0)",
+        "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+        "Mozilla/5.0 (Linux; U; Android 3.0; en-us; sdk Build/HONEYCOMB) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13",
+        "Mozilla/5.0 (Linux; U; Android 4.0.3; en-us; google_sdk Build/MR1) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; GTB7.4; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; BRI/2; .NET4.0C; .NET4.0E; McAfee)",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/8.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/19.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0",
+        "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405",
+        "Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B367 Safari/531.21.10",
+        "Mozilla/5.0 (X11; CrOS armv7l 2913.260.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.99 Safari/537.11"]
+   return agents[rand(agents.count)]
+end
+
+def cleanlink(link)
+  return (link && link[0..3] == 'http') ? link : false;
+end
+
+def parselinks(links)
+  linkstospider = Array.new
+  links.each do |link|
+    puts link
+    addlink = nil
+    if link["href"] && link["href"].index('?url=http') && link["href"].index("webcache.google") == nil
+      addlink = link["href"].split('?url=')[1]
+    elsif link["href"] && link["href"].index('url?q=http') && link["href"].index("webcache.google") == nil
+      addlink = link["href"].split('url?q=')[1]
+    end
+    if addlink && cleanlink(addlink) 
+      linkstospider.push(addlink)
+    end
+  end
+  if linkstospider.count < 1
+    puts "Nothing Google like"
+    links.each do |link|
+      puts "Examining #{link['href']}"
+      if link["href"] && link["href"].index('google') == nil
+        if cleanlink(link["href"]) 
+          linkstospider.push(link["href"])
+        end
+      end
+    end
+  end
+  puts "\nParsed to:"
+  linkstospider.each do |slink|
+    puts slink
+  end
+  return linkstospider.count > 0 ? linkstospider[rand(linkstospider.count)] : ''
+end
+
+linuxwords = '/usr/share/dict/linux.words'
+macwords = '/usr/share/dict/words'
+
+location = (true && File.exists?(linuxwords)) || macwords
+
+words = initialize_database(location)
 randword = words[rand(words.count)].strip
 print "Random word is #{randword}\n"
 
-requrl = "http://www.google.com/search?q=#{randword}&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-US:unofficial&client=firefox-a"
-response = open(requrl, 'User-Agent' => 'RandomBrowser').read
-puts response
+requrl = "https://www.google.com/search?q=#{randword}&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-US:unofficial&client=firefox-a"
+response = Nokogiri::HTML(open(requrl, 'User-Agent' => randomagentstring).read)
+links = response.css("a")
+randlink = parselinks(links)
+if randlink == ''
+  abort('No links')
+end
+
+randagent = randomagentstring
+puts "Spidering #{randlink} as #{randagent}"
+begin
+  spideresponse = open(randlink, 'User-Agent' => randagent)
+rescue OpenURI::HTTPError => error
+  spideresponse = error.io
+end
+puts "Got #{spideresponse.status[0]}"
+
